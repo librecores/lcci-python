390c0d7056d3fe64b5c3b5729a38f64b31945b4e
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-05-29 14:31:42 +0200

    Start main function with version

--- lcci/main.py
+++ lcci/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
+import argparse
+from lcci import __version__
+
 def main():
-    pass
+     parser = argparse.ArgumentParser()
+
+     parser.add_argument('--version', help='Display the FuseSoC version', action='version', version=__version__)
+
+     parsed_args = parser.parse_args()
+     if hasattr(parsed_args, 'func'):
+         run(parsed_args)
+     else:
+         parser.print_help()
 
 if __name__ == "__main__":
     main()
